11212c916197ca748f3dbe3132b0547e4fbd82f2
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-08-31 11:32:59 +0200

    add aggiunto disegno, oggetto che permette di disegnare e animare

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+  //  impostazione canvas
+  const canvas = document.getElementById("canvas1");
+
+  //   disegno: un oggetto che contiene tutti i metodi e le proprieta che permettono di disegnare e animare colori,forme e altre grafiche nell'elemento canvas
+  const disegno = canvas.getDisegno("2d");
+});
